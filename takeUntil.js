6f72365458a6c6a8bc83707afb3009247fbd75fe
egesenli <egesenli@gmail.com>
2022-11-02 02:41:15 -0400

    Updated and improved takeUntil function.

--- takeUntil.js
+++ takeUntil.js
@@ -28,6 +28,7 @@ const takeUntil = function(array, callback) {
     const resultCallback = callback(array[index]);
     if (resultCallback) {
       untilIndex = index - 1;
+      break;
     }
   }
   for (let i = 0; i <= untilIndex; i++) {
