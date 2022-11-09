a115f3852150789c33e37e4ed9af1a547775f0bc
egesenli <egesenli@gmail.com>
2022-11-09 17:05:39 -0500

    Refactored middle function.

--- middle.js
+++ middle.js
@@ -1,33 +1,8 @@
-const assertArraysEqual = function(actual, expected) {
-  if (actual.toString() === expected.toString()) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-const eqArrays = function(actual, expected) {
-  if (actual.toString() === expected.toString()) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-// Array.prototype.middle = function () {
-//   const
-//       half = Math.floor(this.length/2)
-//       offset = 1 - this.length % 2;
-  
-//   return this.slice(half - offset, half + 1);
-// };
-
 const middle = function(array) {
   let half = Math.floor(array.length/2);
   let offset = 1 - array.length % 2;
   return array.slice(half - offset, half + 1);
 };
 
-console.log(middle([1, 2, 3, 4, 5]));    // [3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // [3, 4]
-console.log(middle([1,2,3,4]));       // [2, 3]
+module.exports = middle;
+
